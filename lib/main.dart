95d4ed85c66489967c263191d5104787e36082a5
Ghaida <ghaida@Ghaidas-MacBook-Pro.local>
2022-10-05 23:37:31 +0300

    todo-first-commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:todo_app/models/task_data.dart';
+import 'screens/tasks_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider (
+      create: (context) => TaskData() ,
+      child: MaterialApp(
+        theme: ThemeData(fontFamily: 'NoticiaText'),
+        home: TasksScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/services/authenticate.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class AuthService {
+  // sign in anon
+
+  // sign in  with email & password
+    // sign in  with email & password
+
+}
